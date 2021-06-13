41347eefedf0edebfb1222e24d8658325f6fb613
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-06-13 18:39:16 +0300

    реализация ДЗ по стрелкам

--- gallery-items.js
+++ gallery-items.js
@@ -64,125 +64,138 @@ const pictures = [
   },
 ];
 
-const galleryEl = document.querySelector('.js-gallery')
+const galleryBox = document.querySelector('.js-gallery')
+const modalBox = document.querySelector('.js-lightbox')
+const modalImage = document.querySelector('.lightbox__image')
 
-const modalContainer = document.querySelector(".js-lightbox")
-const modalImg = document.querySelector(".lightbox__image")
 
-const imgEl = galleryGenerator(pictures);
 
-galleryEl.insertAdjacentHTML('afterbegin', imgEl)
+// Создаем разметку
 
-/* Генератор разметки*/
-
-function galleryGenerator(pictures) {
-  return pictures.map(({ preview, original, description }) => {
-    return ` <li class= "gallery__item">
-      <a
+const createImg = pictures.map(({preview, original,description}, index) => {
+  return `
+    <li class="gallery__item">
+  <a
     class="gallery__link"
-    href="${original}"
+    href="${preview}"
   >
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
+      data-index="${index}"
     />
   </a>
 </li>`
-  }
-  
-  ).join("")
-}
 
-const openModal = galleryEl.addEventListener('click', onImageClick)
+}).join("")
 
-/* Открытие модалки с правльной картинкой*/
+galleryBox.innerHTML = createImg
 
-function onImageClick(evt) {
 
+// Открытие модалки по клику
+
+galleryBox.addEventListener('click', onImageClik)
+
+function onImageClik(evt) {
   evt.preventDefault()
-  
-  const imgEl = evt.target.classList.contains('gallery__image')
 
-  if (!imgEl) {
+const galleryImg = evt.target.classList.contains('gallery__image')
+  if (!galleryImg) {
  return
   }
-  const imgURL = evt.target.dataset.source
-  const imgALT = evt.target.getAttribute("alt")
   
-  modalContainer.classList.add ("is-open")
+  modalBox.classList.add("is-open")
+  
 
-  setImgAtributes(imgURL, imgALT)
+  // Получаем и устанавливаем атрибуты для полной каринки 
+  const imgURL = evt.target.dataset.source
+  const imgAlt = evt.target.getAttribute('alt')
+  const imgIndex = evt.target.dataset.index
+ 
+  setAtributes (imgURL, imgAlt, imgIndex)
+ 
 
-  window.addEventListener('keydown', (evt) => {
+  // Закрытие модалки по нажатию эскейп
+    window.addEventListener("keydown", (evt) => {
     if (evt.code === 'Escape') {
-      modalContainer.classList.remove("is-open")
-    }
+     modalBox.classList.remove("is-open")
+   }
   })
-}
 
-/* Поулчаем атрибуты для картинки в модалке*/
+// Смена картинки по нажатию стрелок на клавиатуре
+  window.addEventListener("keydown", imageChange) 
+     
+  
+    }
 
-function setImgAtributes(url, alt) {
-  modalImg.setAttribute("src", `${url}`)
-  modalImg.setAttribute("alt", `${alt}`) 
-}
 
-/* Закрытие модалки*/
-modalContainer.addEventListener('click', closeModal)
+// Закрытие модалки
+modalBox.addEventListener('click', closeModal)
 
 function closeModal(evt) {
 
-  const isModalOpen = modalContainer.classList.contains("is-open")
-  if (!isModalOpen) {
+  const modalImg = evt.target.classList.contains('lightbox__image')
+  if (modalImg) {
     return
   }
-  else if (evt.target === modalImg) {
-    return
-  };
 
-  modalContainer.classList.remove("is-open")
-  
-   /* Закртые по клику на кнопку ескейп */
-  window.removeEventListener("keydown")
-    
-    atributesCleaning()
+  // Снятие слушателей событий по кнопкам
+   window.removeEventListener("keydown", (evt) => {
+    if (evt.code === 'Escape') {
+     modalBox.classList.remove("is-open")
+   }
+  })
+
+    window.removeEventListener("keydown", imageChange) 
+
+  modalBox.classList.remove("is-open")
+
+
+modalImage.setAttribute("src", " " )
+modalImage.setAttribute("alt", " ")
+
+
 
 }
 
-/* Чистим атрибуты*/
+// Смена картинок
+function imageChange (evt) {
+       
+  const currentImg = document.querySelector('.lightbox__image')
+  const currentImgIndex = currentImg.dataset.index
 
-function atributesCleaning() {
-  modalImg.setAttribute("src", " ")
-  modalImg.setAttribute("alt", " ")
-}
-
-window.addEventListener('keydown', imageSwitch)
-
-function imageSwitch(evt) {
-  const isModalOpen = modalContainer.classList.contains("is-open")
-  if (!isModalOpen) {
-    return
-  }
-  
-  if (evt.code === "ArrowRight") {
-
-    for (let i = 0; i < pictures.length - 1; i++) {
-      if (modalImg.src === pictures[i].original) {
-        modalImg.src = pictures[(i += 1)].original
-      }
-    }
-  }
-
-    if (evt.code === "ArrowLeft") {
-
-      for (let i = 1; i < pictures.length; i++) {
-        if (modalImg.src === pictures[i].original) {
-          modalImg.src = pictures[(i-=1)].original
-        }
+  if (evt.code === 'ArrowRight') {
       
-      }
-      }
-    }
+    if (currentImgIndex < pictures.length - 1) {
+      const nextImgIndex = Number(currentImgIndex) + 1
+      const nextImg = document.querySelector(`.gallery__image[data-index="${nextImgIndex}"]`)
+      const nextImgUrl = nextImg.dataset.source
+      const nextImgAlt = nextImg.getAttribute("alt")
 
+     setAtributes (nextImgUrl, nextImgAlt, nextImgIndex)
+    }
+  }
+    
+  if (evt.code === 'ArrowLeft') {
+
+    if (currentImgIndex > 0) {
+      const prevImgIndex = Number(currentImgIndex) - 1
+
+      const prevImg = document.querySelector(`.gallery__image[data-index="${prevImgIndex}"]`)
+      const prevImgUrl = prevImg.dataset.source
+      const prevImgAlt = prevImg.getAttribute("alt")
+
+    setAtributes(prevImgUrl, prevImgAlt, prevImgIndex)
+    }
+  }
+}
+
+
+function setAtributes(imgURL, imgAlt, imgIndex) {
+      
+   modalImage.setAttribute("src", `${imgURL}` )
+  modalImage.setAttribute("alt", `${imgAlt}`)
+  modalImage.setAttribute("data-index", `${imgIndex}`)
+    }
